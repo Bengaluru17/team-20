b22c8f07d174fc98859aa528661731537e95a455
Rachana Ramesh <rachanaramesh662288@gmail.com>
2017-07-09 00:32:11 +0530

    Create fpage.php

---
+++ fpage.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  
+</head>
+<style>
+.jumbotron {
+    background-color: #f4511e; /* Orange */
+    color: #ffffff;
+}
+</style>
+<body>
+<div class="container">
+<a class="navbar-brand" > <img src="final_logo-02.png" left="100" width="250" height="150"> </a>
+
+  <div class="jumbotron">
+    <h1 style="padding-left:250px;">Tutorial Sessions</h1>      
+          
+</div>
+
+<div class="row">
+  <div class="col-sm-4">
+  Welcome user,
+  </div>
+  <div class="col-sm-8"><h1 style="text-align:center;"center">Test 1</h1></div>
+  </div>
+  
+</body>
+</html>
