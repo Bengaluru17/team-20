3cd0fe93197e7ec5a04261150b5be1c83f449ecb
ravivats <vats.ravi96@gmail.com>
2017-07-08 22:49:10 +0530

    uploaded fifth activity

---
+++ app/src/main/java/com/example/halfbloodprince/amoghhelperapp/FifthTrainingActivity.java
@@ -0,0 +1,13 @@
+package com.example.halfbloodprince.amoghhelperapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FifthTrainingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fifth_training);
+    }
+}
